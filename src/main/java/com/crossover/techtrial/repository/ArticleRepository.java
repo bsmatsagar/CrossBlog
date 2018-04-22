eb1c1baa5e698a328d627343e6eeb17785ad7a51
Babasaheb Shivaji Matsagar <bsmatsagar@gmail.com>
2018-04-22 23:04:30 +0530

    Update ArticleRepository.java
    add in CrudRepository

--- src/main/java/com/crossover/techtrial/repository/ArticleRepository.java
+++ src/main/java/com/crossover/techtrial/repository/ArticleRepository.java
@@ -1,13 +1,13 @@
 package com.crossover.techtrial.repository;
 
 import java.util.List;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.crossover.techtrial.model.Article;
 
 @RepositoryRestResource(exported = false)
-public interface ArticleRepository extends PagingAndSortingRepository<Article, Long> {
+public interface ArticleRepository extends CrudRepository<Article, Long> {
 
 	List<Article> findTop10ByTitleContainingIgnoreCase(String title);
 
